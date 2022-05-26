589e5f8257b91f0ae74bf05afcf264b9e64f63b3
nwd0907 <nwd0907@newbizstart.com>
2022-05-26 13:16:29 +0900

    Update pagination-with-lifting-stat-up

---
+++ class/pages/14-03-pagination-last/index.tsx
@@ -0,0 +1,89 @@
+import { useQuery, gql } from "@apollo/client";
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+const FETCH_BOARDS_COUNT = gql`
+  query fetchBoardsCount {
+    fetchBoardsCount
+  }
+`;
+
+const MyRow = styled.div`
+  display: flex;
+`;
+
+const MyColumn = styled.div`
+  width: 25%;
+`;
+
+export default function MapBoardPage() {
+  const [startPage, setStartPage] = useState(1);
+  const { data, refetch } = useQuery(FETCH_BOARDS);
+  const { data: dataBoardsCount } = useQuery(FETCH_BOARDS_COUNT);
+  const lastPage = Math.ceil(dataBoardsCount?.fetchBoardsCount / 10);
+
+  const onClickPage = (event) => {
+    refetch({ page: Number(event.target.id) });
+  };
+
+  const onClickPrevPage = () => {
+    if (startPage === 1) return;
+    setStartPage((prev) => prev - 10);
+    refetch({ page: startPage - 10 });
+  };
+
+  const onClickNextPage = () => {
+    if (startPage + 10 <= lastPage) {
+      setStartPage((prev) => prev + 10);
+      refetch({ page: startPage + 10 });
+    }
+  };
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el: any) => (
+        <MyRow key={el._id}>
+          <MyColumn>{el.writer}</MyColumn>
+          <MyColumn>{el.title}</MyColumn>
+        </MyRow>
+      ))}
+      <span onClick={onClickPrevPage}>이전페이지</span>
+      {new Array(10).fill(1).map(
+        (_, index) =>
+          index + startPage <= lastPage && (
+            <span
+              key={index + startPage}
+              id={String(index + startPage)}
+              onClick={onClickPage}
+            >
+              {" "}
+              {index + startPage}
+            </span>
+          )
+      )}
+      <span onClick={onClickNextPage}>다음페이지</span>
+      {/* {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => (
+        <span key={index + 1} id={String(index + 1)} onClick={onClickPage}> {index + 1} </span>
+      ))} */}
+
+      {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(el => (
+            <span key={el} id={String(el)} onClick={onClickPage}> {el} </span>
+        ))} */}
+
+      {/* <span id="1" onClick={onClickPage}> 1 </span>
+      <span id="2" onClick={onClickPage}> 2 </span>
+      <span id="3" onClick={onClickPage}> 3 </span> */}
+    </div>
+  );
+}
