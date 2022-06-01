85b463e1266b96aaa8dc6c0edc0f80444127a9d6
nwd0907 <nwd0907@newbizstart.com>
2022-06-01 19:48:42 +0900

    Update firebase

--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -6,6 +6,24 @@ import Layout from "../src/components/commons/layout";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalStyles";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIpZgTNTkevkL8nIT6ynHi11UXIAKFMa0",
+  authDomain: "codecamp-frontend-07.firebaseapp.com",
+  projectId: "codecamp-frontend-07",
+  storageBucket: "codecamp-frontend-07.appspot.com",
+  messagingSenderId: "71811250762",
+  appId: "1:71811250762:web:9278b13d0524a5f66d44b3",
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
     uri: "http://backend07.codebootcamp.co.kr/graphql",
--- class_backend/index.ts
+++ class_backend/index.ts
@@ -2,6 +2,71 @@ console.log("타입스크립트를 실행했어요!!!");
 
 import { DataSource } from "typeorm";
 import { Board } from "./Board.postgres";
+import { ApolloServer, gql } from "apollo-server";
+
+// REST: express
+// GRAPHQL: ApolloServer
+
+// 1. 타입 - typeDefs
+const myTypeDefs = gql`
+  input CreateBoardInput {
+    writer: String
+    title: String
+    contents: String
+  }
+
+  type Board {
+    number: Int
+    writer: String
+    title: String
+    contents: String
+  }
+
+  type Query {
+    fetchBoards: [Board]
+  }
+
+  type Mutation {
+    # createBoard(writer: String, title: String, contents: String): String # - 연습용(example)
+    createBoard(createBoardInput: CreateBoardInput): String # - 실무용(backend07)
+  }
+`;
+
+// 2. 함수(API) - resolvers
+const myResolvers = {
+  Query: {
+    fetchBoards: async () => {
+      const result = await Board.find();
+      return result;
+    },
+  },
+
+  Mutation: {
+    createBoard: async (_: any, args: any) => {
+      await Board.insert({
+        ...args.createBoardInput,
+        // writer: args.createBoardInput.writer,
+        // title: args.createBoardInput.title,
+        // contents: args.createBoardInput.contents
+      });
+
+      // 수정하면?
+      // Board.update({writer: "철수"}, {title: "제목2"})
+
+      // 삭제하면?
+      // Board.delete({writer: "철수"})
+      // Board.update({writer: "철수"}, {deletedAt: new Date()})
+
+      return "게시물 등록에 성공했습니다!!";
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: myTypeDefs,
+  resolvers: myResolvers,
+  cors: true,
+});
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -18,6 +83,16 @@ const AppDataSource = new DataSource({
 AppDataSource.initialize()
   .then(() => {
     console.log("연결성공!!!");
+
+    // 백엔드 API를 리슨(24시간동안 접속가능하게 대기상태로 만들어주기)
+    server
+      .listen(4000)
+      .then(() => {
+        console.log("서버 실행 성공!!!");
+      })
+      .catch(() => {
+        console.log("서버 실행 실패!!!");
+      });
   })
   .catch(() => {
     console.log("연결실패!!!");
