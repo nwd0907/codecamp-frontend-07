ac2243ef6f9bb453fe93a124d5a0c68ddd6915a1
nwd0907 <nwd0907@newbizstart.com>
2022-07-03 17:49:22 +0900

    Update ssr

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   generateBuildId: () => "codecamp_deploy_07",
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -1,12 +1,50 @@
 import { useRouter } from "next/router";
+import { gql, request } from "graphql-request";
+import Head from "next/head";
 
-export default function BoardsDetailPage() {
+export default function BoardsDetailPage(props) {
   const router = useRouter();
 
   return (
     <div>
-      안녕하세요! 게시글 상세페이지 입니다!!!, 게시글 ID는
-      {router.query.boardId} 입니다!!!
+      <Head>
+        <meta property="og:title" content={props?.fetchBoard.title} />
+        <meta property="og:description" content={props?.fetchBoard.contents} />
+        <meta property="og:image" content={props?.fetchBoard.images?.[0]} />
+      </Head>
+      <div>
+        안녕하세요! 게시글 상세페이지 입니다!!!, 게시글 ID는
+        {router.query.boardId} 입니다!!!
+      </div>
     </div>
   );
 }
+
+const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      title
+      contents
+      images
+    }
+  }
+`;
+
+export const getServerSideProps = async (context) => {
+  // 여기서 fetchBoard 요청하기
+  const result = await request(
+    "https://backend07.codebootcamp.co.kr/graphql",
+    FETCH_BOARD,
+    { boardId: context.query.boardId }
+  );
+
+  return {
+    props: {
+      fetchBoard: {
+        title: result.fetchBoard.title,
+        contents: result.fetchBoard.contents,
+        images: result.fetchBoard.images,
+      },
+    },
+  };
+};
